0c1823e1ea15ca8777bb65620ef55731ecfca095
Aninoss II <aninosspro@gmial.com>
2023-04-09 19:09:04 +0100

    Start Working on the RuleEngine

--- Week_1/Week_1/main.cpp
+++ Week_1/Week_1/main.cpp
@@ -3,64 +3,90 @@
 
 using namespace std;
 
-class GoodClass
+struct BoardSquare
 {
-
-	string _str;
-public:
-	GoodClass(int id, string name, int age) : _id(id), _name(name), _age(age)
+	enum E
 	{
-		
-	}
-protected:
-	int _id;
-	string _name;
-	int _age;
+		X,
+		O,
+		Empty
+	};
 };
 
-class BadClass : public GoodClass
+class Board
 {
-	string _str;
+	int _width;
+	BoardSquare::E* _squares;
 public:
-	BadClass(string str, int id, string name, int age) : GoodClass(id, name, age)
+	Board(int width) : _width(width)
 	{
-		this->_str = str;
+		_squares = new BoardSquare::E[width * width];
+		for (auto i = 0; i < GetTotalSquares(); i++)
+			_squares[i] = BoardSquare::Empty;
+	}
+
+	~Board()
+	{
+		delete[] _squares;
+	}
+
+	int GetWidth() { return _width; }
+	int GetTotalSquares() { return _width * _width; }
+
+	BoardSquare::E GetSquare(int index)
+	{
+		return _squares[index];
+	}
+	void SetSquare(int index, BoardSquare::E square)
+	{
+		this->_squares[index] = square;
 	}
 };
 
-/*
-class ClassB
+class IRuleEngine
 {
 public:
-	ClassB(int number)
-	{
-		
-	}
+	virtual BoardSquare::E HasWon(Board board) = 0;
 };
 
-class ClassA
+class Game
 {
+	Board& _board;
+	IRuleEngine& _iRuleEngine;
+public:
+	Game( Board& board, IRuleEngine& iRuleEngine): _board(board), _iRuleEngine(iRuleEngine)
+	{		
+	}
+	BoardSquare::E Run()
+	{		
+	}
+
 private:
-	int& _number;
-	int& _number2;
-	int& _number3 ;
-	ClassB classB;
-public:
-	ClassA(int& number) : _number(number) , _number2(number), _number3(number), classB(10)
+	void Render()
 	{
-		
+		for(int i = 1; i < _board.GetTotalSquares(); i++)
+		{
+			cout << GetSquareChar(i, _board.GetSquare(i - 1)) << " ";
+			if ((i % _board.GetWidth()) == 0)
+				cout << "\n";
+		}
 	}
-	int getNumber() { return _number; }
-	int getNumber2() { return _number2; }
-	int getNumber3() { return _number3; }
+
+	char GetSquareChar(int index, BoardSquare::E square)
+	{
+		switch(square)
+		{
+		case BoardSquare::X: return 'X';
+		case BoardSquare::O: return 'O';
+		}
+
+		return index + '0';
+	}
+
 };
-*/
 
 int main()
 {
-	/*int number = 10;
-	ClassA* classA = new ClassA(number);
-	cout << "Number: " << classA->getNumber() << " | Number2: " << classA->getNumber2() << " | Number3: " << classA->getNumber3() << endl;*/
 
 
 	return 0;
